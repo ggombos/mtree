bf64783cb276df15c73496be098dc7f64b974153
Sajti Zsolt Zoltán <sajtizsolt@gmail.com>
2021-10-11 13:16:12 +0200

    Made testing easier

---
+++ data/fingerprint_create_tables.sql
@@ -0,0 +1,32 @@
+DROP EXTENSION IF EXISTS mtree_gist CASCADE;
+CREATE EXTENSION mtree_gist;
+
+DROP TABLE IF EXISTS Fingerprint_1000 CASCADE;
+CREATE TABLE Fingerprint_1000 (
+  id        SERIAL PRIMARY KEY,
+  song_fk   INTEGER,
+  hash	    TEXT,
+  offset2   INTEGER,
+  hash_gist mtree_text
+);
+COPY Fingerprint_1000 FROM '/home/postgres/fingerprint_data_1000';
+
+DROP TABLE IF EXISTS Fingerprint_10000 CASCADE;
+CREATE TABLE Fingerprint_10000 (
+  id        SERIAL PRIMARY KEY,
+  song_fk   INTEGER,
+  hash	    TEXT,
+  offset2   INTEGER,
+  hash_gist mtree_text
+);
+COPY Fingerprint_10000 FROM '/home/postgres/fingerprint_data_10000';
+
+DROP TABLE IF EXISTS Fingerprint_1002156 CASCADE;
+CREATE TABLE Fingerprint_1002156 (
+  id        SERIAL PRIMARY KEY,
+  song_fk   INTEGER,
+  hash	    TEXT,
+  offset2   INTEGER,
+  hash_gist mtree_text
+);
+COPY Fingerprint_1002156 FROM '/home/postgres/fingerprint_data_1002156';
---
+++ script/copy_test_data.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+readonly SOURCE_DATA_DIRECTORY="/home/zsolt/DATA/Development/mtree_gist/data"
+readonly TARGET_DATA_DIRECTORY="/home/postgres"
+
+function copy_file() {
+  cp "${SOURCE_DATA_DIRECTORY}/$1" "${TARGET_DATA_DIRECTORY}/$1"
+  chown postgres "${TARGET_DATA_DIRECTORY}/$1"
+}
+
+copy_file "1000.csv"
+copy_file "fingerprint_create_indexes.sql"
+copy_file "fingerprint_create_tables.sql"
+copy_file "fingerprint_data_1000"
+copy_file "fingerprint_data_10000"
+copy_file "fingerprint_data_1002156"
+copy_file "fingerprint_measure.sql"
