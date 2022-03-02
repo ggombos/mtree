1dfe2f97e17bc1766cc43b12c9e60f5fc9252cbe
Zsolt Zoltán Sajti <sajtizsolt@gmail.com>
2022-03-02 07:09:28 +0100

    Slightly refactored scripts

--- script/copy_test_data.sh
+++ script/copy_test_data.sh
@@ -6,25 +6,47 @@ readonly SOURCE_SCRIPT_DIRECTORY="/home/zsolt/DATA/Development/mtree_gist/script
 readonly SOURCE_DATA_DIRECTORY="/home/zsolt/DATA/Development/mtree_gist/data"
 readonly TARGET_DATA_DIRECTORY="/home/postgres"
 
+readonly DATA_FILENAMES=(
+  "1000_create_indexes.sql"
+  "1000_create_tables.sql"
+  "1000_measure.sql"
+  "1000.csv"
+  "2000_int.csv"
+  "fingerprint_create_indexes.sql"
+  "fingerprint_create_tables.sql"
+  "fingerprint_data_10"
+  "fingerprint_data_1000"
+  "fingerprint_data_10000"
+  "fingerprint_data_1002156"
+  "fingerprint_measure.sql"
+  "int8_measure.sql"
+  "mtreeOrderbyhiba.sql"
+)
+
+readonly SCRIPT_FILENAMES=(
+  "start.sh"
+  "stop.sh"
+)
+
 function copy_file() {
   cp "$1/$2" "${TARGET_DATA_DIRECTORY}/$2"
   chown postgres "${TARGET_DATA_DIRECTORY}/$2"
 }
 
-copy_file "${SOURCE_SCRIPT_DIRECTORY}" "start.sh"
-copy_file "${SOURCE_SCRIPT_DIRECTORY}" "stop.sh"
+function copy_data_file() {
+  copy_file "${SOURCE_DATA_DIRECTORY}" "$1"
+}
 
-copy_file "${SOURCE_DATA_DIRECTORY}" "1000_create_indexes.sql"
-copy_file "${SOURCE_DATA_DIRECTORY}" "1000_create_tables.sql"
-copy_file "${SOURCE_DATA_DIRECTORY}" "1000_measure.sql"
-copy_file "${SOURCE_DATA_DIRECTORY}" "1000.csv"
-copy_file "${SOURCE_DATA_DIRECTORY}" "2000_int.csv"
-copy_file "${SOURCE_DATA_DIRECTORY}" "fingerprint_create_indexes.sql"
-copy_file "${SOURCE_DATA_DIRECTORY}" "fingerprint_create_tables.sql"
-copy_file "${SOURCE_DATA_DIRECTORY}" "fingerprint_data_10"
-copy_file "${SOURCE_DATA_DIRECTORY}" "fingerprint_data_1000"
-copy_file "${SOURCE_DATA_DIRECTORY}" "fingerprint_data_10000"
-copy_file "${SOURCE_DATA_DIRECTORY}" "fingerprint_data_1002156"
-copy_file "${SOURCE_DATA_DIRECTORY}" "fingerprint_measure.sql"
-copy_file "${SOURCE_DATA_DIRECTORY}" "int8_measure.sql"
-copy_file "${SOURCE_DATA_DIRECTORY}" "mtreeOrderbyhiba.sql"
+function copy_script_file() {
+  copy_file "${SOURCE_SCRIPT_DIRECTORY}" "$1"
+}
+
+for filename in "${DATA_FILENAMES[@]}";
+do
+  copy_data_file "${filename}"
+done
+
+for filename in "${SCRIPT_FILENAMES[@]}";
+do
+  copy_script_file "${filename}"
+done
--- script/install.sh
+++ script/install.sh
@@ -7,29 +7,34 @@ readonly POSTGRESQL_INCLUDE_DIRECTORY="/usr/include/postgresql/server"
 readonly POSTGRESQL_EXTENSION_DIRECTORY="/usr/share/postgresql/extension"
 readonly POSTGRESQL_LIBRARY_DIRECTORY="/usr/lib/postgresql"
 
+readonly FILENAMES=(
+  "mtree_text"
+  "mtree_text_util"
+  "mtree_text_array"
+  "mtree_text_array_util"
+  "mtree_int8"
+  "mtree_int8_util"
+  "mtree_int8_array"
+  "mtree_int8_array_util"
+#  "mtree_float"
+#  "mtree_float_util"
+  "mtree_float_array"
+  "mtree_float_array_util"
+  "mtree_util"
+  "mtree_gist"
+)
+
+function compile_file() {
+  cc -fPIC -c -I "${POSTGRESQL_INCLUDE_DIRECTORY}" "${SOURCE_DIRECTORY}/$1.c" -o "${SOURCE_DIRECTORY}/$1.o"
+}
+
+function remove_file() {
+  rm "${SOURCE_DIRECTORY}/$1.o"
+}
+
 function create_and_copy_so() {
-  cc -fPIC -c -I "${POSTGRESQL_INCLUDE_DIRECTORY}" "${SOURCE_DIRECTORY}/mtree_text.c" -o "${SOURCE_DIRECTORY}/mtree_text.o"
-  cc -fPIC -c -I "${POSTGRESQL_INCLUDE_DIRECTORY}" "${SOURCE_DIRECTORY}/mtree_text_util.c" -o "${SOURCE_DIRECTORY}/mtree_text_util.o"
-  cc -fPIC -c -I "${POSTGRESQL_INCLUDE_DIRECTORY}" "${SOURCE_DIRECTORY}/mtree_int8.c" -o "${SOURCE_DIRECTORY}/mtree_int8.o"
-  cc -fPIC -c -I "${POSTGRESQL_INCLUDE_DIRECTORY}" "${SOURCE_DIRECTORY}/mtree_int8_util.c" -o "${SOURCE_DIRECTORY}/mtree_int8_util.o"
-  cc -fPIC -c -I "${POSTGRESQL_INCLUDE_DIRECTORY}" "${SOURCE_DIRECTORY}/mtree_int8_array.c" -o "${SOURCE_DIRECTORY}/mtree_int8_array.o"
-  cc -fPIC -c -I "${POSTGRESQL_INCLUDE_DIRECTORY}" "${SOURCE_DIRECTORY}/mtree_int8_array_util.c" -o "${SOURCE_DIRECTORY}/mtree_int8_array_util.o"
-  cc -fPIC -c -I "${POSTGRESQL_INCLUDE_DIRECTORY}" "${SOURCE_DIRECTORY}/mtree_text_array.c" -o "${SOURCE_DIRECTORY}/mtree_text_array.o"
-  cc -fPIC -c -I "${POSTGRESQL_INCLUDE_DIRECTORY}" "${SOURCE_DIRECTORY}/mtree_text_array_util.c" -o "${SOURCE_DIRECTORY}/mtree_text_array_util.o"
-  cc -fPIC -c -I "${POSTGRESQL_INCLUDE_DIRECTORY}" "${SOURCE_DIRECTORY}/mtree_util.c" -o "${SOURCE_DIRECTORY}/mtree_util.o"
-  cc -fPIC -c -I "${POSTGRESQL_INCLUDE_DIRECTORY}" "${SOURCE_DIRECTORY}/mtree_gist.c" -o "${SOURCE_DIRECTORY}/mtree_gist.o"
   cc -shared -o "${SOURCE_DIRECTORY}/mtree_gist.so" "${SOURCE_DIRECTORY}/mtree_gist.o" "${SOURCE_DIRECTORY}/mtree_text.o" "${SOURCE_DIRECTORY}/mtree_text_util.o" "${SOURCE_DIRECTORY}/mtree_int8.o" "${SOURCE_DIRECTORY}/mtree_int8_util.o" "${SOURCE_DIRECTORY}/mtree_util.o"
   cp "${SOURCE_DIRECTORY}/mtree_gist.so" "${POSTGRESQL_LIBRARY_DIRECTORY}/mtree_gist.so"
-  rm "${SOURCE_DIRECTORY}/mtree_text.o"
-  rm "${SOURCE_DIRECTORY}/mtree_text_util.o"
-  rm "${SOURCE_DIRECTORY}/mtree_int8.o"
-  rm "${SOURCE_DIRECTORY}/mtree_int8_util.o"
-  rm "${SOURCE_DIRECTORY}/mtree_int8_array.o"
-  rm "${SOURCE_DIRECTORY}/mtree_int8_array_util.o"
-  rm "${SOURCE_DIRECTORY}/mtree_text_array.o"
-  rm "${SOURCE_DIRECTORY}/mtree_text_array_util.o"
-  rm "${SOURCE_DIRECTORY}/mtree_util.o"
-  rm "${SOURCE_DIRECTORY}/mtree_gist.o"
   rm "${SOURCE_DIRECTORY}/mtree_gist.so"
 }
 
@@ -41,5 +46,16 @@ function copy_sql_and_control() {
   rm "${SOURCE_DIRECTORY}/mtree_gist_tmp.control"
 }
 
+for filename in "${FILENAMES[@]}";
+do
+  compile_file "${filename}"
+done
+
 create_and_copy_so
+
+for filename in "${FILENAMES[@]}";
+do
+  remove_file "${filename}"
+done
+
 copy_sql_and_control
