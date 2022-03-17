8e051114c96be5ae241306998bec7ef368ba35a9
ggombos <ggombos16@gmail.com>
2022-03-17 14:28:32 +0000

    bad text metric is slow and wrong + opclass usage example

--- data/other/millionSong.sql
+++ data/other/millionSong.sql
@@ -19,7 +19,7 @@ CREATE TABLE millionSong (
 -- \copy millionSong FROM '/home/ggombos/mtree/mtree_gist/data/millionSongSrc/millionSongTags.csv' DELIMITER ';' CSV HEADER;
 \copy millionSong FROM '/home/ggombos/mtree/mtree_gist/data/millionSongSrc/million100e.csv' DELIMITER ';' CSV HEADER;
 
-CREATE INDEX millionSong_gist_index ON millionSong USING gist (tags mtree_text_array_opclass) ;
+CREATE INDEX millionSong_gist_index ON millionSong USING gist (tags mtree_text_array_opclass(distancestrategy='1',picksplitstrategy="SamplingMinOverlapArea")) ;
 
 SET enable_seqscan = off;
 
